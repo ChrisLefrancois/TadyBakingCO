c2429280b71dd0808c2b5eef7b2f46238d52bb83
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2025-07-21 21:09:56 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-green-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-700">Tailwind is Working!</h1>
+    </div>
+  );
+}
+
+export default App;
